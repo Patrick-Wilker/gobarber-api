2894775f7ce4bc278227bbaeb225d9f8a98064c6
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-08-14 15:25:53 -0300

    feat: send appointment cancellation email

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+
+export default{
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+        user: "27e70a609ac45d",
+        pass: "fab2ab4e070b50",
+    },
+    default: {
+        from: 'Equipe Gobarber <noreply@gobarber.com>'
+    },
+}
